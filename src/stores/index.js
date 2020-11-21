f08a0367dca046a35934060b1bffe99553c5db29
熊静松 <8238536+xiong-jingsong@user.noreply.gitee.com>
2020-11-21 23:13:01 +0800

    add @bale/plugin-proposal-decorators解决装饰器报错

---
+++ src/stores/index.js
@@ -0,0 +1,8 @@
+import React, { createContext, useContext } from 'react'
+import { AuthStore } from './auth'
+
+const context = createContext({
+    AuthStore: new AuthStore()
+})
+const useStores = () => useContext(context)
+export { useStores }
