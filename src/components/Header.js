02808d6213608de16f6c28afb20b7b8f0b7d2d4e
熊静松 <8238536+xiong-jingsong@user.noreply.gitee.com>
2020-11-22 10:40:51 +0800

    添加login/logout按钮

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom'
+
 import logo from './logo.svg'
 import styled from 'styled-components';
+
 
 const StyledHeader = styled.header`
    display:flex;
@@ -12,18 +14,31 @@ const StyledHeader = styled.header`
         height:30px;
         border:1px solid red;
     }
-    a{
-        color:#fff;
-        margin-left:30px;
-        &.xxx{
-            color:red;
-            border-bottom:1px solid red;
+    nav{
+        flex:1;
+        a{
+            color:#fff;
+            margin-left:30px;
+            &.xxx{
+                color:red;
+                border-bottom:1px solid red;
+            }
+        }
+    }
+    .login{
+        border:1px solid red;
+        button:first-child{
+            margin-right:5px;
+        }
+        button{
+            padding:10px 30px;
         }
     }
 `
 
 
 function Header() {
+    const router = useHistory()
     return (
         <StyledHeader>
             <img src={logo} alt="xxx" />
@@ -32,6 +47,16 @@ function Header() {
                 <NavLink to="/about" activeClassName="xxx" >关于</NavLink>
                 <NavLink to="/history" activeClassName="xxx" >历史</NavLink>
             </nav>
+            <div className="login">
+                <button onClick={
+                    () => router.push('/login')
+                }>登陆</button>
+                <button onClick={
+                    () => {
+                        router.push('/register')
+                    }
+                }>注册</button>
+            </div>
         </StyledHeader>
     )
 }
